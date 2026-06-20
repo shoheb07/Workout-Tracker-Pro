d394dc174db2da807c0ec158245af21a8c684a49
shoheb mulla <149918582+shoheb07@users.noreply.github.com>
2026-06-20 21:42:00 +0530

    Create bmi_calculator.py

---
+++ bmi_calculator.py
@@ -0,0 +1,15 @@
+def calculate_bmi(weight, height):
+    height = height / 100
+
+    bmi = weight / (height ** 2)
+
+    if bmi < 18.5:
+        category = "Underweight"
+    elif bmi < 25:
+        category = "Normal"
+    elif bmi < 30:
+        category = "Overweight"
+    else:
+        category = "Obese"
+
+    return round(bmi, 2), category
